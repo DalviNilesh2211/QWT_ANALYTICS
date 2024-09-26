d5f22e004905f88ea55cab5f652ce6b049740b59
Nilesh Dalvi <nilesh.dalvi@cognizant.com>
2024-09-26 06:32:32 +0000

    shipment schema

--- models/staging/stg_shipments.sql
+++ models/staging/stg_shipments.sql
@@ -1,4 +1,4 @@
-{{config(materialized='table')}}
+{{config(materialized='table'), schema = env_var('DBT_STAGE_SCHEMA','STAGING')}}
 select OrderID , LineNo , ShipperID , CustomerID ,
        ProductID , EmployeeID ,
        split_part(ShipmentDate ,' ',1 )::date ShipmentDate, 
