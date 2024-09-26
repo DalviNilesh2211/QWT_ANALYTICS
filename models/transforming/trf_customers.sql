badce0be7b9833b518703baa44d1c0a0b289093b
Nilesh Dalvi <nilesh.dalvi@cognizant.com>
2024-09-26 06:14:50 +0000

    Transform Schema

--- models/transforming/trf_customers.sql
+++ models/transforming/trf_customers.sql
@@ -1,10 +1,8 @@
 {{
     config(
-        materialized='table', schema='transforming', pre_hook=['grant select on table staging.stg_customers to role public']
+        materialized='table', schema= env_var('DBT_TRNASFORMING_SCHEMA','transforming') , pre_hook=['grant select on table staging.stg_customers to role public']
     )
 }}
 select CUSTOMERID, COMPANYNAME, CONTACTNAME, CITY, COUNTRY, DIVISIONID, ADDRESS, FAX, 
        PHONE, POSTALCODE, case when STATEPROVINCE='' then 'NA' else STATEPROVINCE end STATEPROVINCE
  from {{ref ( 'stg_customers')}}
-
- 
